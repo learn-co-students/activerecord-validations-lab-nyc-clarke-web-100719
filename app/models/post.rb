fed4d6051306cd85704f7367ace6d6da1a7c79c7
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-11-05 16:49:52 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbaity?
+
+    @@clickbait_phrases = [    
+        /won't believe/i,
+        /secret/i,
+        /top [0-9]*/i,
+        /guess/i
+    ]
+
+
+    def is_clickbaity?
+        if @@clickbait_phrases.none? { |bait| bait.match title }
+          errors.add(:title, "Not clickbaity enough")
+        end
+    end
 end
